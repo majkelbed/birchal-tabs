aee7c530e4751e3e8e8cd9ca0a698849560217f8
Michał Bednarczyk <bednarczyk.business@gmail.com>
2022-07-01 19:24:36 +0200

    feat: create swapi client

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { BrowserRouter } from "react-router-dom";
-import { App } from './modules/app/app';
-import reportWebVitals from './reportWebVitals';
+import { App } from "./modules/app/app";
+import reportWebVitals from "./reportWebVitals";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
